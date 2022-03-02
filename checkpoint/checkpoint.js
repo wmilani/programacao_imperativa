947995e0c1868f4729078a580bb80ec6f5dec324
wmilani <milani.weslley@live.com>
2022-03-02 18:40:11 -0300

    Check point

--- checkpoint/checkpoint.js
+++ checkpoint/checkpoint.js
@@ -13,3 +13,70 @@
 */
 
 
+const comida1 = "pipoca";
+const tempoPipoca = 31;
+
+tempoPipoca >= 10 && tempoPipoca <= 19
+? console.log("Prato pronto")
+: tempoPipoca <= 9
+? console.log("Tempo insuficiente")
+: tempoPipoca >= 20 && tempoPipoca <=29
+? console.log("Seu prato queimou")
+: tempoPipoca >= 30
+? console.log("Kaaabum")
+: console.log("Prato pronto, bom apetite!!!")
+
+const comida2 = "macarrao";
+const tempoMacarrao = 23;
+
+tempoMacarrao >= 8 && tempoMacarrao <= 15
+? console.log("Prato pronto")
+: tempoMacarrao <= 8
+? console.log("Tempo insuficiente")
+: tempoMacarrao >= 16 && tempoMacarrao<= 23
+? console.log("Seu prato queimou")
+: tempoMacarrao >= 24
+? console.log("Kaaabum")
+: console.log("Prato pronto, bom apetite")
+
+
+const comida3 = "carne";
+const tempoCarne = 14;
+
+tempoCarne >= 15 && tempoCarne <= 29
+? console.log("Prato pronto")
+: tempoCarne <= 15
+? console.log("Tempo insuficiente")
+: tempoCarne >= 30 && tempoCarne <= 44
+? console.log("Seu prato queimou")
+: tempoCarne >= 45
+? console.log("Kaaabum")
+: console.log("Prato pronto, bom apetite")
+
+const comida4 = "feijao";
+const tempoFeijao = 12;
+
+tempoFeijao >= 12 && tempoFeijao <= 23
+? console.log("Prato pronto")
+: tempoFeijao <= 12
+? console.log("Tempo insuficiente")
+: tempoFeijao >= 24 && tempoFeijao <= 35
+? console.log("Seu prato queimou")
+: tempoFeijao >= 36
+? console.log("Kaaabum")
+: console.log("Prato pronto, bom apetite")
+
+const comida5 = "brigadeiro"
+const tempoBrigadeiro = 8;
+
+tempoBrigadeiro >= 8 && tempoBrigadeiro <= 15
+? console.log("Prato pronto")
+: tempoBrigadeiro <= 7
+? console.log("Tempo insuficiente")
+: tempoBrigadeiro >= 16 && tempoBrigadeiro <= 23
+? console.log("Seu prato queimou")
+: tempoBrigadeiro >= 24
+? console.log("Kaaabum")
+: console.log("Prato pronto, bom apetite")
+
+
